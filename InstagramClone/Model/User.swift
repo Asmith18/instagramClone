836936b498aaa8e7e8f2bb5b68e606e550ed96da
Adam Smith <Smittysadam@gmail.com>
2024-12-30 18:34:41 -0600

    after 2 days i finally fixed a bug on searchview.

---
+++ InstagramClone/Model/User.swift
@@ -0,0 +1,28 @@
+//
+//  User.swift
+//  InstagramClone
+//
+//  Created by Adam Smith on 12/29/24.
+
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    var username: String
+    var profileImageName: String?
+    var fullName: String?
+    var bio: String?
+    let email: String
+}
+
+extension User {
+    static var MOCK_USERS: [User] = [
+        .init(id: NSUUID().uuidString, username: "Adam", profileImageName: "ruffles", fullName: "Adam Smith", bio: "Hello World", email: "adam@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Adam", profileImageName: "ruffles", fullName: "Adam Smith", bio: "Hello World", email: "adam@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Adam", profileImageName: "ruffles", fullName: "Adam Smith", bio: "Hello World", email: "adam@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Adam", profileImageName: "ruffles", fullName: "Adam Smith", bio: "Hello World", email: "adam@gmail.com"),
+    ]
+}
+
+
