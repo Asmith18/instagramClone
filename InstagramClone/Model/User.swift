ccffb47673414f2779086c68832dfec7b571527f
Adam Smith <Smittysadam@gmail.com>
2025-01-06 19:02:38 -0600

    fetching all our users from firebase database in our search view and displaying the data for each user in the profile view.

--- InstagramClone/Model/User.swift
+++ InstagramClone/Model/User.swift
@@ -18,10 +18,10 @@ struct User: Identifiable, Hashable, Codable {
 
 extension User {
     static var MOCK_USERS: [User] = [
-        .init(id: NSUUID().uuidString, username: "Adam432456", profileImageName: "ruffles", fullName: "Adam", bio: "Hello World!", email: "adam@gmail.com"),
-        .init(id: NSUUID().uuidString, username: "Ed Bolian", profileImageName: "revulto", fullName: "Ed", bio: "Founder of VinWiki", email: "BigEd@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Adam1234", profileImageName: "ruffles", fullName: "Adam", bio: "Hello World!", email: "adam@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Ed.Bolian", profileImageName: "revulto", fullName: "Ed", bio: "Founder of VinWiki", email: "Ed@gmail.com"),
         .init(id: NSUUID().uuidString, username: "Supercar_Ron", profileImageName: "ron", fullName: "Ron", bio: "Supercar Collector", email: "Ron@gmail.com"),
-        .init(id: NSUUID().uuidString, username: "Adam LZ", profileImageName: "adamlz", fullName: "Adam", bio: "Best Drifter in the world!", email: "adamlz@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "AdamLZ", profileImageName: "adamlz", fullName: "Adam", bio: "Best Drifter in the world!", email: "lz@gmail.com"),
     ]
 }
 
